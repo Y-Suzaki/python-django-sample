76946eb3715c259bfa522b16fb2440aa792b5cdc
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-01-13 22:09:14 +0900

    indexページの修正

--- pyFirst/myapp/urls.py
+++ pyFirst/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'myapp'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('hello', views.hello, name='hello')
 ]
--- pyFirst/myapp/views.py
+++ pyFirst/myapp/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
+    context = {
+        'login_name': 'guest'
+    }
+    return render(request, 'myapp/index.html', context)
+
+
+def hello(request):
     return HttpResponse('Hello Django.')
+
