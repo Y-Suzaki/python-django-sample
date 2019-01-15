627756a6fc85a654cc86c3f7d065b6d3e9c291b8
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-01-15 12:49:43 +0900

    登録画面の追加

--- pyFirst/diary/views.py
+++ pyFirst/diary/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import DayCreateForm
 
 
 def index(request):
@@ -7,3 +8,21 @@ def index(request):
         'login_name': 'guest'
     }
     return render(request, 'diary/day_list.html', context)
+
+
+def add(request):
+    """ add
+    初回時はGET、Form送信時はPOSTで本メソッドが呼ばれる。
+    """
+    form = DayCreateForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('diary:index')
+
+    context = {
+        'login_name': 'guest',
+        'form': DayCreateForm()
+    }
+    return render(request, 'diary/day_form.html', context)
+
